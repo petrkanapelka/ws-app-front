b235ebf44a933642cc86b99a17d89519c4a3f431
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-11-30 13:18:06 +0300

    Refactors UI with Material UI components
    Replaces basic HTML elements with Material UI components for enhanced styling and user experience. Improves form handling and layout.  Removes unnecessary comments and improves code readability.

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,10 @@ import {
 } from './chat-reducer';
 import { debounce } from 'lodash';
 import { useForm } from 'react-hook-form';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+import { Box, Divider, Typography } from '@mui/material';
+import AppTheme from './theme/AppTheme';
 
 export type User = {
   id: string;
@@ -37,7 +41,6 @@ function App() {
 
   const chatContainerRef = useRef<HTMLDivElement | null>(null);
 
-  // Initialize connection
   useEffect(() => {
     dispatch(createConnection());
     return () => {
@@ -45,7 +48,6 @@ function App() {
     };
   }, [dispatch]);
 
-  // Auto-scroll when messages update
   useEffect(() => {
     if (isScrollMode) {
       chatContainerRef.current?.scrollTo({
@@ -55,7 +57,6 @@ function App() {
     }
   }, [isScrollMode, messages]);
 
-  // Scroll handler
   const handleScroll = () => {
     if (chatContainerRef.current) {
       const { scrollTop, scrollHeight, clientHeight } = chatContainerRef.current;
@@ -64,7 +65,6 @@ function App() {
     }
   };
 
-  // Debounced typing stop handler
   const debouncedStopTyping = useRef(
     debounce(() => {
       dispatch(userStopTyping());
@@ -87,79 +87,95 @@ function App() {
 
   const renderMessages = () =>
     messages.map(({ id, user, message }) => (
-      <div className={styles.message} key={id}>
-        <b>{user.name}: </b>
-        <span>{message}</span>
-        <hr />
-      </div>
+      <>
+        <Box className={styles.message} key={id}>
+          <b>{user.name}: </b>
+          <span>{message}</span>
+        </Box>
+        <Divider />
+      </>
     ));
 
   const renderTypingUsers = () =>
-    typingUsers.map((u) => <span key={u.id}>{u.name} is typing..., </span>);
+    typingUsers.map((u) => <Typography variant='caption' key={u.id}>{u.name} is typing..., </Typography>);
 
   return (
-    <div className={styles.App}>
-      <div className={styles['parent-container']}>
-        <div className={styles['chat-container']} ref={chatContainerRef} onScroll={handleScroll}>
-          {renderMessages()}
-        </div>
-        {renderTypingUsers()}
+    <AppTheme>
+      <Box className={styles.App}>
+        <Box className={styles['parent-container']}>
+          <form onSubmit={nicknameForm.handleSubmit(handleSendName)} className={styles['input-container']}>
+            <Box>
+              <TextField
+                type="text"
+                label="Enter nickname"
+                variant="outlined"
+                error={!!nicknameForm.formState.errors.nickname}
+                helperText={nicknameForm.formState.errors.nickname?.message}
+                {...nicknameForm.register('nickname', {
+                  required: 'Nickname is required',
+                  maxLength: {
+                    value: 10,
+                    message: 'Nickname cannot exceed 10 characters',
+                  },
+                  minLength: {
+                    value: 2,
+                    message: 'Nickname must be at least 2 characters',
+                  },
+                })}
+                aria-invalid={!!nicknameForm.formState.errors.nickname}
+              />
+            </Box>
+            <Button variant="contained" type="submit">SET</Button>
+          </form>
+          <Box className={styles['chat-container']} ref={chatContainerRef} onScroll={handleScroll}>
+            {renderMessages()}
+          </Box>
+          <Box className={styles.typing}>
+            {renderTypingUsers()}
+          </Box>
 
-        {/* Nickname Form */}
-        <form onSubmit={nicknameForm.handleSubmit(handleSendName)} className={styles['input-container']}>
-          <div>
-            <input
-              type="text"
-              placeholder="Enter nickname"
-              {...nicknameForm.register('nickname', {
-                required: 'Nickname is required',
-                maxLength: {
-                  value: 10,
-                  message: 'Nickname cannot exceed 10 characters',
-                },
-                minLength: {
-                  value: 2,
-                  message: 'Nickname must be at least 2 characters',
-                },
-              })}
-              aria-invalid={nicknameForm.formState.errors.nickname ? 'true' : 'false'}
-            />
-            {nicknameForm.formState.errors.nickname && (
-              <p role="alert">{nicknameForm.formState.errors.nickname.message}</p>
-            )}
-          </div>
-          <button type="submit">ENTER NICKNAME</button>
-        </form>
 
-        {/* Message Form */}
-        <form onSubmit={messageForm.handleSubmit(handleSendMessage)} className={styles['input-container']}>
-          <div>
-            <textarea
-              rows={3}
-              placeholder="Enter message"
-              onKeyDown={handleTyping}
-              onKeyUp={debouncedStopTyping}
-              {...messageForm.register('message', {
-                required: 'Message is required',
-                maxLength: {
-                  value: 100,
-                  message: 'Message cannot exceed 100 characters',
-                },
-                minLength: {
-                  value: 2,
-                  message: 'Message must be at least 2 characters',
-                },
-              })}
-              aria-invalid={messageForm.formState.errors.message ? 'true' : 'false'}
-            />
-            {messageForm.formState.errors.message && (
-              <p role="alert">{messageForm.formState.errors.message.message}</p>
-            )}
-          </div>
-          <button type="submit">SEND MESSAGE</button>
-        </form>
-      </div>
-    </div>
+
+          <form onSubmit={messageForm.handleSubmit(handleSendMessage)} className={styles['input-container']}>
+            <Box sx={{width: '100%'}}>
+              <TextField
+                id="outlined-basic"
+                label="Enter message"
+                variant="outlined"
+                onKeyDown={handleTyping}
+                onKeyUp={debouncedStopTyping}
+                {...messageForm.register('message', {
+                  required: 'Message is required',
+                  maxLength: {
+                    value: 100,
+                    message: 'Message cannot exceed 100 characters',
+                  },
+                  minLength: {
+                    value: 2,
+                    message: 'Message must be at least 2 characters',
+                  },
+                })}
+                aria-invalid={!!messageForm.formState.errors.message}
+                error={!!messageForm.formState.errors.message}
+                helperText={
+                  messageForm.formState.errors.message?.message || ' '
+                }
+                sx={{
+                  width: '90%',
+                  // height: '100px',
+                }}
+                InputProps={{
+                  sx: {
+                    height: '100px',
+                  },
+                }}
+              />
+            </Box>
+            <Button variant="contained" type="submit">SEND</Button>
+          </form>
+        </Box>
+      </Box>
+    </AppTheme>
   );
 }
 
