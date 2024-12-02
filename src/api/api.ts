7dfad5011710a71d5e775c487c9a7c0afcff7ac7
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-12-02 22:52:00 +0300

    Adds socket.io connection handling
    Implements API functions to create, destroy, and subscribe to socket events.
    
    Handles connection errors and provides methods for sending user names and messages, as well as managing typing status.  Establishes connection to local development server via websockets.

---
+++ src/api/api.ts
@@ -0,0 +1,55 @@
+import { io, Socket } from 'socket.io-client';
+import { Message, User } from '../types/types';
+
+// export const SOCKET_URL = process.env.REACT_APP_CLIENT_URL; /* || 'http://localhost:3000' */
+// export const SOCKET_URL = 'https://ws-app-back.onrender.com/';
+export const SOCKET_URL = 'http://localhost:3010';
+
+export const socketApi = {
+    socket: null as Socket | null,
+    createConnection() {
+        this.socket = io(SOCKET_URL, {
+            transports: ['websocket'],
+            reconnection: true,
+        });
+        this.socket.on('connect_error', (err) => {
+            console.error('Connection error:', err);
+        });
+    },
+    destroyConnection() {
+        this.socket?.disconnect();
+        this.socket = null;
+    },
+
+    subscribe(
+        initMessagesHandler: (messages: Message[]) => void,
+        newMessageSentHandler: (message: Message) => void,
+        userTypingHandler: (user: User) => void,
+        userStopTypingHandler: (user: User) => void,
+        errorHandler: (error: string) => void
+    ) {
+        if (this.socket) {
+            this.socket.on('init-messages-published', initMessagesHandler);
+            this.socket.on('new-message-sent', newMessageSentHandler);
+            this.socket.on('user-typing', userTypingHandler);
+            this.socket.on('user-stop-typing', userStopTypingHandler);
+            this.socket.on('error-message', errorHandler);
+        }
+    },
+
+    sendName(name: string) {
+        this.socket?.emit('client-name-sent', name);
+    },
+
+    sendMessage(message: string) {
+        this.socket?.emit('client-message-sent', message);
+    },
+
+    userTyping() {
+        this.socket?.emit('user-typed');
+    },
+
+    userStopTyping() {
+        this.socket?.emit('user-stop-typed');
+    },
+};
