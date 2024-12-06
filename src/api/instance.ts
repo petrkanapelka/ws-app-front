25868c1354fe8378200f0dc7de5bf4845d82ecd5
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-12-07 01:08:10 +0300

    Adds authentication to API requests
    Includes authorization headers in API requests using the token from local storage.
    
    Adds new API methods for user registration, login, authentication, and logout.

--- src/api/instance.ts
+++ src/api/instance.ts
@@ -1,15 +1,27 @@
 import axios from 'axios';
 import { SOCKET_URL } from './api';
 
+const token = localStorage.getItem('token');
+
 export const instance = axios.create({
     baseURL: SOCKET_URL,
+    headers: {
+        Authorization: token ? `Bearer ${token}` : '',
+    },
+    // withCredentials: true,
 });
 
 export const axiosApi = {
-    registerUser(authData: { email: string; password: string, name: string}) {
+    registerUser(authData: { email: string; password: string; name: string }) {
         return instance.post('/register', authData);
     },
     loginUser(authData: { email: string; password: string }) {
         return instance.post('/login', authData);
+    },
+    authUser(authData: { token: string }) {
+        return instance.post('/profile', authData);
+    },
+    logOut(authData: { token: string }) {
+        return instance.post('/logout', authData);
     },
 };
