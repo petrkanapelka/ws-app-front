54a95160b0c9c7fb7470001c1506b97f90d14967
Petr Kanapelka <kanapelka.petr@gmail.com>
2024-12-07 01:09:28 +0300

    Implements logout functionality
    Adds a logout button and handler to the navigation bar.  Clears local storage and updates the login state in the Redux store upon logout.  Navigates user to the login page after logout.

--- src/components/MenuAppBar.tsx
+++ src/components/MenuAppBar.tsx
@@ -5,21 +5,30 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import { useNavigate } from 'react-router-dom';
 import { useAppDispatch, useAppSelector } from '../store';
-import { setUserLogin } from '../chat-reducer';
+import { logout, setUserLogin } from '../chat-reducer';
 
 export default function ButtonAppBar() {
     const navigate = useNavigate()
     const dispatch = useAppDispatch()
+
     const isUserLogin = useAppSelector(state => state.chat.isUserLogin)
+
+    const logoutHandler = () => {
+        dispatch(logout());
+        navigate('/login');
+        localStorage.removeItem('token');
+        dispatch(setUserLogin(false));
+    };
+
     return (
-        <Box sx={{ flexGrow: 1, width: '100%', backgroundColor: '#333B4D', height: '10vh', display: 'flex', alignItems: 'center' }}>
-            <AppBar sx={{ backgroundColor: '#333B4D', height: '10vh' }}>
+        <Box sx={{ flexGrow: 1, width: '100%', backgroundColor: '#333B4D', height: '7vh', display: 'flex', alignItems: 'center' }}>
+            <AppBar sx={{ backgroundColor: '#333B4D', height: '7vh' }}>
                 <Toolbar sx={{ display: 'flex', alignItems: 'center' }}>
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
                         Rapid Chat
                     </Typography>
                     {isUserLogin ?
-                        <Button color="inherit" onClick={() => { navigate('/register'); dispatch(setUserLogin(false)) }}>Logout</Button>
+                        <Button color="inherit" onClick={logoutHandler}>Logout</Button>
                         : <>
                             <Button color="inherit" onClick={() => { navigate('/register') }}>Register</Button>
                             <Button color="inherit" onClick={() => { navigate('/login') }}>Login</Button>
